e0de1383e9304d43fec4d1020563f07899eefa7d
andreasmartenss <amarte24@student.aau.dk>
2026-03-30 20:45:59 +0200

    Update wellDonePage.dart

--- flutter_application_1/lib/View/wellDonePage.dart
+++ flutter_application_1/lib/View/wellDonePage.dart
@@ -34,7 +34,7 @@ class UIPage extends StatefulWidget {
 
 class _UIPageState extends State<UIPage> {
 
-void _UIbutton() {
+  void _UIbutton() {
     setState(() {
       
     });
@@ -55,105 +55,105 @@ void _UIbutton() {
     super.dispose();
   }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: Stack(
-      children: [
-        Align(
-          alignment:Alignment.topCenter,
-          child: Padding(
-            padding: const EdgeInsets.only(top: 80),
-            
-          child: Column(
-            children: const [
-              Text('WELL DONE!', style: TextStyle(fontSize: 40)),
-             ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Align(
+            alignment:Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 80),
+              
+            child: Column(
+              children: const [
+                Text('WELL DONE!', style: TextStyle(fontSize: 40)),
+              ],
+              ),
             ),
           ),
-        ),
 
-        Align(
-          child: Column(
-            mainAxisAlignment: .center,
-            children: const [
-              Text("ROUND 1:", style: TextStyle(fontSize: 25)),
-              Text(" ", style: TextStyle(fontSize: 20)),
-              Text("ROUND 2:", style: TextStyle(fontSize: 25)),
-              Text(" ", style: TextStyle(fontSize: 20)),
-              Text("ROUND 3:", style: TextStyle(fontSize: 25)),
-              Text(" ", style: TextStyle(fontSize: 20)),
-              Text("ROUND 4:", style: TextStyle(fontSize: 25)),
-              Text(" ", style: TextStyle(fontSize: 20)),
-              Text("ROUND 5:", style: TextStyle(fontSize: 25)),
-            ],
+          Align(
+            child: Column(
+              mainAxisAlignment: .center,
+              children: const [
+                Text("ROUND 1:", style: TextStyle(fontSize: 25)),
+                Text(" ", style: TextStyle(fontSize: 20)),
+                Text("ROUND 2:", style: TextStyle(fontSize: 25)),
+                Text(" ", style: TextStyle(fontSize: 20)),
+                Text("ROUND 3:", style: TextStyle(fontSize: 25)),
+                Text(" ", style: TextStyle(fontSize: 20)),
+                Text("ROUND 4:", style: TextStyle(fontSize: 25)),
+                Text(" ", style: TextStyle(fontSize: 20)),
+                Text("ROUND 5:", style: TextStyle(fontSize: 25)),
+              ],
+            ),
           ),
-        ),
-      
-        Align(
-          alignment: Alignment.bottomCenter,
-          child: Padding(
-            padding: const EdgeInsets.all(130),
-            child: LiquidGlassLayer(
-              settings: const LiquidGlassSettings(
-                blur: 10,
-                thickness: 20,
-                glassColor: Color.fromARGB(255, 139, 207, 95), 
-              ),
-              child: LiquidGlass(
-                shape: LiquidRoundedSuperellipse(borderRadius: 30),
-                child: SizedBox(
-                  width: 200,
-                  height: 60,
-                  child: ElevatedButton(
-                    onPressed: _UIbutton,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 110, 184, 58), 
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(30),
+        
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(130),
+              child: LiquidGlassLayer(
+                settings: const LiquidGlassSettings(
+                  blur: 10,
+                  thickness: 20,
+                  glassColor: Color.fromARGB(255, 139, 207, 95), 
+                ),
+                child: LiquidGlass(
+                  shape: LiquidRoundedSuperellipse(borderRadius: 30),
+                  child: SizedBox(
+                    width: 200,
+                    height: 60,
+                    child: ElevatedButton(
+                      onPressed: _UIbutton,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color.fromARGB(255, 110, 184, 58), 
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
                       ),
+                      child: const Text("FINISH", style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 23
+                        ),
+                        ),
                     ),
-                    child: const Text("FINISH", style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 23
-                      ),
-                      ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-         Align(
-            alignment: Alignment.topLeft,
-            child: ConfettiWidget(
-              confettiController: _controller,
-              blastDirection: pi / 4,
-              emissionFrequency: 0.05,
-              numberOfParticles: 20,
-              maxBlastForce: 20,
-              minBlastForce: 5,
-              gravity: 0.3,
-              shouldLoop: false,
+          Align(
+              alignment: Alignment.topLeft,
+              child: ConfettiWidget(
+                confettiController: _controller,
+                blastDirection: pi / 4,
+                emissionFrequency: 0.05,
+                numberOfParticles: 20,
+                maxBlastForce: 20,
+                minBlastForce: 5,
+                gravity: 0.3,
+                shouldLoop: false,
+              ),
             ),
-          ),
-          
-            Align(
-            alignment: Alignment.topRight,
-            child: ConfettiWidget(
-              confettiController: _controller,
-              blastDirection: pi / 1,
-              emissionFrequency: 0.05,
-              numberOfParticles: 20,
-              maxBlastForce: 20,
-              minBlastForce: 5,
-              gravity: 0.3,
-              shouldLoop: false,
+            
+              Align(
+              alignment: Alignment.topRight,
+              child: ConfettiWidget(
+                confettiController: _controller,
+                blastDirection: pi / 1,
+                emissionFrequency: 0.05,
+                numberOfParticles: 20,
+                maxBlastForce: 20,
+                minBlastForce: 5,
+                gravity: 0.3,
+                shouldLoop: false,
+              ),
             ),
-          ),
-      ],
-    ),
-  );
-}
+        ],
+      ),
+    );
+  }
 }
