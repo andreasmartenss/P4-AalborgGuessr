9f1cd279866eb10676e49fe4cda69c2e110e71dd
andreasmartenss <amarte24@student.aau.dk>
2026-04-22 11:18:06 +0200

    goddag

--- flutter_application_1/lib/ViewModel/database.dart
+++ flutter_application_1/lib/ViewModel/database.dart
@@ -1,34 +1,19 @@
 // ignore_for_file: unnecessary_lambdas
 
 import "dart:async";
+import 'package:pocketbase/pocketbase.dart';
 
-import "package:pocketbase/pocketbase.dart";
+final pb = PocketBase('http://130.225.39.250');
+
+Future<void> fetchData() async {
+  final records = await pb.collection('photos_and_geopoint').getFullList();
+  
+  for (final record in records) {
+    print(record.id);
+    print(record.data); // all fields
+  }
+}
 
 void main() {
-  final pb = PocketBase("http://127.0.0.1:8090");
-
-  // fetch a paginated list with "example" records
-  pb.collection("example").getList(page: 1, perPage: 10).then((result) {
-    // success...
-    print("Result: $result");
-  }).catchError((dynamic error) {
-    // error...
-    print("Error: $error");
-  });
-
-  // listen to realtime connect/reconnect events
-  pb.realtime.subscribe("PB_CONNECT", (e) {
-    print("Connected: $e");
-  });
-
-  // subscribe to realtime changes in the "example" collection
-  pb.collection("example").subscribe("*", (e) {
-    print(e.action); // create, update, delete
-    print(e.record); // the changed record
-  });
-
-  // unsubsribe from all "example" realtime subscriptions after 10 seconds
-  Timer(const Duration(seconds: 10), () {
-    pb.realtime.unsubscribe(); // unsubscribe from all realtime events
-  });
+  fetchData();
 }
