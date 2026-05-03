596a489eaee50e72bfa5a8e55f65a9b1c4318188
andreasmartenss <amarte24@student.aau.dk>
2026-05-03 14:58:46 +0200

    min flutter virker igen. jeg er ikke blind nu

--- flutter_application_1/lib/features/location/view/test_location_page.dart
+++ flutter_application_1/lib/features/location/view/test_location_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:flutter/features/location/viewmodel/gps_location_vm.dart';
+import 'package:flutter_application_1/features/location/viewmodel/gps_location_vm.dart';
+
 
 class TestLocationPage extends StatelessWidget {
   const TestLocationPage({super.key});
