85753434ce1ba0f25f85a2f4487f589f1ccc8e3e
andreasmartenss <amarte24@student.aau.dk>
2026-05-03 12:04:42 +0200

    Update database.dart

--- flutter_application_1/lib/core/services/database.dart
+++ flutter_application_1/lib/core/services/database.dart
@@ -8,18 +8,18 @@ class PocketBaseService {
   );
 
 
-Future<void> fetchData() async {
-  final records = await pb.collection('photos_and_geopoint').getFullList();
-  
-  for (final record in records) {
-    final imageFile = record.data['photos'];
-    final imageUrl = 'http://130.225.39.250/api/files/${record.collectionId}/${record.id}/$imageFile?thumb=0x0'; //Thumb bruges til at konvertere billedet til en mindre størrelse, hvilket gør det hurtigere at hente og vise i appen
-    print(imageUrl); // paste this URL in a browser to verify it loads
-    print(record.data);
+  Future<void> fetchData() async {
+    final records = await pb.collection('photos_and_geopoint').getFullList();
+    
+    for (final record in records) {
+      final imageFile = record.data['photos'];
+      final imageUrl = 'http://130.225.39.250/api/files/${record.collectionId}/${record.id}/$imageFile?thumb=0x0'; //Thumb bruges til at konvertere billedet til en mindre størrelse, hvilket gør det hurtigere at hente og vise i appen
+      print(imageUrl); // paste this URL in a browser to verify it loads
+      print(record.data);
+    }
   }
-}
 
-void main() {
-  fetchData();
-  }
+  void main() {
+    fetchData();
+    }
 }
