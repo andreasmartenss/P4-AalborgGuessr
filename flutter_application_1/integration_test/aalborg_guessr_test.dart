bf709d92f03e87d5a3328f455213be14cbdc7ea3
andreasmartenss <amarte24@student.aau.dk>
2026-05-13 21:12:39 +0200

    Update aalborg_guessr_test.dart

--- flutter_application_1/integration_test/aalborg_guessr_test.dart
+++ flutter_application_1/integration_test/aalborg_guessr_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
 import 'package:flutter_application_1/main.dart' as app;
-import 'package:flutter/material.dart';
 
 void main() {
   testWidgets('full game flow — 5 rounds then return to home', (tester) async {
