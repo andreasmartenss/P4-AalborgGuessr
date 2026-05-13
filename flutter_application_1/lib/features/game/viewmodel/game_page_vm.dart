0577e4ce1430e48c2d74e1d497bb9638470bba02
andreasmartenss <amarte24@student.aau.dk>
2026-05-13 21:02:02 +0200

    dokumentation til gamepageVM

--- flutter_application_1/lib/features/game/viewmodel/game_page_vm.dart
+++ flutter_application_1/lib/features/game/viewmodel/game_page_vm.dart
@@ -6,11 +6,16 @@ import 'package:pocketbase/pocketbase.dart';
 import 'dart:async';
 import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 
+// This class is s a view-model for the game, that fetches elements from the geo model, photo selecter model and the score model
+// and turn it into data for the view to show.
 class GamePageVM extends ChangeNotifier {
+
+  // Bbjects from the models initialized 
   final GeoModel _geoModel = GeoModel();
   final PhotoPickerModel _photoModel = PhotoPickerModel();
   final ScorepageModel _scorepageModel = ScorepageModel();
 
+  // Attributes from the score, inidividual rounds, total rounds, the amount of time and navigations to different pages.
   int score = 0;
   int currentRound = 1;
   int totalRounds = 5;
@@ -18,18 +23,24 @@ class GamePageVM extends ChangeNotifier {
   bool navigateToScore = false;
   bool navigateToWellDone = false;
 
+  // Attributes that takes the distance in meters from the geo model.
   double? distanceInMeters;
+  // List of integers of the scores of individual rounds.
   final List<int> _roundScores = [];
+  // a getter for the list.
   List<int> get roundScores => _roundScores;
 
+  // Built in timer in dart.
   Timer? _timer;
 
+  // This method formats the time in the rounds, and parses the integers into strings.
   String get formattedTime {
     int minutes = timeUsage ~/ 60;
     int seconds = timeUsage % 60;
     return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
   }
 
+  // This method starts the timer from zero and increments the timer by one second.
   void startTimer() {
     _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
       timeUsage++;
@@ -37,8 +48,10 @@ class GamePageVM extends ChangeNotifier {
     });
   }
 
+  // Fetches the pictures from the Pocketbase.
   RecordModel? pictures;
 
+  // Getter that takes the longtitude and latitude from the pictures. These values gets parsed into strings.
   GeoPoint? get location {
     final raw = pictures?.data['location'];
     if (raw == null) return null;
@@ -47,11 +60,13 @@ class GamePageVM extends ChangeNotifier {
     return GeoPoint(latitude: lat, longitude: lon);
   }
 
+  // Promise, that uses the randomizer to select an unused photo.
   Future<void> getPicture() async {
     pictures = await _photoModel.pickUnseenPhoto();
     notifyListeners();
   }
 
+  // Promise, that initializes the selected photo, the timer, distance.
   Future<void> startRound() async {
     if (currentRound == 1) {
       _photoModel.reset();
@@ -64,18 +79,22 @@ class GamePageVM extends ChangeNotifier {
     notifyListeners();
   }
 
+  // This method sets the location of where the player guessed from.
   void setGuessLocation(GeoPoint guessedPoint) {
     final correct = location;
+
+    // If-statement that states if the photos location is equal to null,than print this in the console.
     if (correct == null) {
       print("DEBUG: correct location er null!");
       return;
     }
+    // Calculaes the distance between the photo and the player in meters using the geo model.
     distanceInMeters = _geoModel.calculateDistance(correct, guessedPoint);
     print("DEBUG: distanceInMeters = $distanceInMeters");
     notifyListeners();
   }
 
-
+  // This method adds the score to the rounds when the player has guessed.
   void addRoundScore() {
     final distance = distanceInMeters ?? 9999.0;
     print("DEBUG: addRoundScore bruger distance = $distance, tid = $timeUsage");
@@ -86,6 +105,7 @@ class GamePageVM extends ChangeNotifier {
     notifyListeners();
   }
 
+  // Promise, that saves all individual rounds. This is for the result page.
   Future<void> saveAllScores() async {
   print("DEBUG: _roundScores ved saveAllScores = $_roundScores");
   if (_roundScores.length < 5) {
@@ -102,6 +122,7 @@ class GamePageVM extends ChangeNotifier {
     );
   }
 
+  // Promise, that increments the rounds until the fith round.
   Future<void> nextRound() async {
     if (currentRound < totalRounds) {
       currentRound++;
@@ -115,23 +136,27 @@ class GamePageVM extends ChangeNotifier {
     notifyListeners();
   }
 
+  // Promise, that allows the user to skip the round.
   Future<void> skipRound() async {
     nextRound();
   }
 
+  // Method that allows the player to leave the game.
   void leaveGame() {
     _timer?.cancel();
     notifyListeners();
   }
 
+  // Method that navigates the user to the score page.
   void onGuess() {
   navigateToScore = true;
   if (currentRound >= totalRounds) {
     _timer?.cancel();
   }
   notifyListeners();
-}
+  }
 
+  // This method stops the timer, and disposes it from the heap.
   @override
   void dispose() {
     _timer?.cancel();
